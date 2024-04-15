d75abe5d056e24be8e84e5f64f8289e629d9d12c
hansikaweerasena <hansikam94@gmail.com>
2024-04-15 14:57:05 -0400

    common function to split and scale data

--- Diabetis_DNN/common.py
+++ Diabetis_DNN/common.py
@@ -33,3 +33,30 @@ def set_seed(seed=42):
     np.random.seed(seed)
     tf.random.set_seed(seed)
     print(f'Seed set to {seed}')
+
+
+def split_and_scale_data(data_df, random_state=42, target='Diabetes_012', test_val_prop=0.1, val_prop=0.5, scaler='MinMax', oneHotEncode=False):
+   
+    # Split the data into features and labels
+    x_all = data.drop('Diabetes_012', axis=1)
+    y_all = data['Diabetes_012']
+
+    if onehot:
+        num_classes = 3
+        y_all = keras.utils.to_categorical(y_all, num_classes)
+
+    # Split the data into training, validation, and test sets
+    train_x, temp_x, train_y, temp_y = train_test_split(x_all, y_all, test_size=test_val_prop, random_state=seed)
+    val_x, test_x, val_y, test_y = train_test_split(temp_x, temp_y, test_size=val_prop, random_state=seed)
+
+    if scaler == 'MinMax':
+        scaler = MinMaxScaler()
+    elif scaler == 'Standard':
+        scaler = StandardScaler()
+    else:
+        raise ValueError('Invalid scaler')
+
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    return X_train, X_test, y_train, y_test 
